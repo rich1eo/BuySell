6b55b646371030f61289ac880e072cc0477daba5
rich1e <v.richkov@gmail.com>
2021-11-29 15:40:09 +0300

    Add admin panel

--- src/main/java/com/example/BuySell/repository/UserRepository.java
+++ src/main/java/com/example/BuySell/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.BuySell.repository;
 import com.example.BuySell.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
